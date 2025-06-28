df2e298d06002bc10c031a8e432fb6bb04131211
Webapptoday <blastingchickens@gmail.com>
2025-06-28 07:09:56 -0500

    Update mychess.py

--- mychess.py
+++ mychess.py
@@ -1,16 +1,12 @@
-import streamlit as st
 import gspread
+import streamlit as st
 from google.oauth2.service_account import Credentials
 
-# Load credentials from Streamlit secrets
-creds_dict = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
+# Load credentials from secrets
+creds_dict = st.secrets["gcp_service_account"]
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 
-# Connect to the Google Sheet
+# Connect to Google Sheets
 client = gspread.authorize(creds)
 sheet = client.open("ChessLegends_Users").sheet1
-
-# Function to append a new row
-def append_row(data):
-    sheet.append_row(data)
