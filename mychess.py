dfd64a6551e5d13cf5951b8d70d753c86d05a896
Webapptoday <blastingchickens@gmail.com>
2025-06-28 07:44:30 -0500

    Update mychess.py

--- mychess.py
+++ mychess.py
@@ -1,13 +1,18 @@
-import gspread
 import streamlit as st
+import gspread
 from google.oauth2.service_account import Credentials
 
 # Load credentials from secrets
-creds_dict = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
-scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+creds_dict = st.secrets["gcp_service_account"]
+scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 
-# Connect to Google Sheets
+# Authorize the client
 client = gspread.authorize(creds)
+
+# Open the spreadsheet by key
 sheet = client.open_by_key("10-YFBO0ILhR3T8aL8j6oBctaCZifKqxcSuNdzmfTNG0").sheet1
 
+# Read and display values
+data = sheet.get_all_records()
+st.write(data)
