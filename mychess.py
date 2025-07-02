38b20d1471dfdb3800a83222d67a6beaef0cc1cb
Webapptoday <blastingchickens@gmail.com>
2025-07-02 09:43:49 -0500

    Update mychess.py

--- mychess.py
+++ mychess.py
@@ -62,12 +62,12 @@ st.markdown('<h2 id="coaches">Meet Our Coaches</h2>', unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("https://i.postimg.cc/5yYQhDY7/Untitled-design-2025-07-02-T093341-074.png", 
+    st.image("https://i.postimg.cc/d3bZwnGq/Untitled-design-2025-07-02-T093312-238.png", 
              caption="Dhairya Mehta (USCF 2150)", use_column_width=True)
     st.write("Over 150 tournaments • Summer camp coach")
 
 with col2:
-    st.image("https://i.postimg.cc/d3bZwnGq/Untitled-design-2025-07-02-T093312-238.png", 
+    st.image("https://i.postimg.cc/5yYQhDY7/Untitled-design-2025-07-02-T093341-074.png", 
              caption="Shouri Mosaliganti (USCF 1700)", use_column_width=True)
     st.write("180+ tournaments • Experienced youth coach")
 
